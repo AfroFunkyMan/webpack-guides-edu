cfe53df1961ec82b4c3d349b4bcc0f49be7ccbce
ruslan <afro.funky.lover@gmail.com>
2018-05-22 17:29:01 +0700

    prepare to Output Management Guide

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
-    var element = document.createElement('div');
+    const element = document.createElement('div');
 
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+
     return element;
-
-    // Add the image to our existing div.
-    var myIcon = new Image();
-    myIcon.src = Icon;
-
-    console.log(Data);
-
-    element.appendChild(myIcon);
 }
 
 document.body.appendChild(component());
