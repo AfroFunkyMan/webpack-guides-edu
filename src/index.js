7d6d9a437be5c28218c2bc55886230ff629a31f2
ruslan <afro.funky.lover@gmail.com>
2018-05-22 17:26:27 +0700

    Loading Data + Loading fonts + img

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
@@ -8,6 +10,14 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
     return element;
+
+    // Add the image to our existing div.
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    console.log(Data);
+
+    element.appendChild(myIcon);
 }
 
 document.body.appendChild(component());
